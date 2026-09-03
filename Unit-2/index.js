2243dc185e54e09ebf24de359c04a6e45f1e9a89
Anshuman2901 <ajha24187@gmail.com>
2026-09-03 12:19:20 +0530

    middleware

--- Unit-2/index.js
+++ Unit-2/index.js
@@ -5,6 +5,34 @@ const app = express(); // Create Express application
 
 const packages = require("./data/tour.json"); // Import packages data
 
+// client --> request --> middleware1 --> middleware2 --> middleware3 --> response
+
+// const middleware1 = (req,res,next)=> {
+//     console.log("Middleware 1");
+//     next();
+// }
+
+// const loggger = (req,res,next)=> {
+//     console.log(`${req.method} ${req.url}`);
+//     next();
+// }
+
+// app.use(loggger);
+
+const CheckAge = (req,res,next)=> {
+    const age = req.query.age;
+    if(age < 18){
+        return res.status(403).json("You are not allowed to access this resource");
+    }
+    next();
+}
+
+app.use(CheckAge); // Use CheckAge middleware for all routes
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+});
+// const tourRoutes = require("./router/tourRoutes"); // Import tour routes
+// app.use("/api/tours", tourRoutes); // Use tour routes for /api/tours endpoint
 
 app.get("/", (req, res) => { // Handle GET request to /
     res.send("Hello World"); // Send response
